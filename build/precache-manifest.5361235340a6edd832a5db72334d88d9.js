682b6b98cc81505ed5af04fdc306c1bf125ccc10
Aya Zayed <ayazayed@std.mans.edu.eg>
2023-03-12 21:10:41 +0200

    please

---
+++ build/precache-manifest.5361235340a6edd832a5db72334d88d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1fe6b6ba984cb09a734",
+    "url": "/static/css/main.ee103797.chunk.css"
+  },
+  {
+    "revision": "f1fe6b6ba984cb09a734",
+    "url": "/static/js/main.f1fe6b6b.chunk.js"
+  },
+  {
+    "revision": "86c9ab519e985715b4c8",
+    "url": "/static/js/1.86c9ab51.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "22453c6630bfc397e0cd6a02ef0079f4",
+    "url": "/index.html"
+  }
+];
